d286d6502519dd567822d52ae5a8376d8ca06c66
Welas <93174143+WilliamsVelas@users.noreply.github.com>
2026-05-04 19:26:20 -0400

    feat(back): bootstrap express server

---
+++ izy-back/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+import productRoutes from "./routes/product.routes";
+import salesRoutes from "./routes/sales.routes";
+import authRoutes from "./routes/auth.routes";
+import cors from "cors";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/products", productRoutes);
+app.use("/api/sales", salesRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
