b21f9accc42256c7930d84d1f0ac49e4f4604be9
Welas <93174143+WilliamsVelas@users.noreply.github.com>
2026-05-04 23:06:08 -0400

    fix: update generateReport endpoint to /api/sales/report

--- izylite/lib/core/network/enpoints.dart
+++ izylite/lib/core/network/enpoints.dart
@@ -2,7 +2,7 @@ class ServerEndpoints {
   static const String login = '/api/auth/login';
 
   static const String sell = '/api/sales';
-  static const String generateReport = '/api/report';
+  static const String generateReport = '/api/sales/report';
 
   static const String product = '/api/products';
   static const String generateSell = '/api/products/sell';
